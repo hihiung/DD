635e5cec21b26c5275762f0484687fd71f75c5b9
hui-ung.kim <hihiung@gmail.com>
2023-06-05 15:28:26 +0900

    READ

--- Project1/DeviceDriver.cpp
+++ Project1/DeviceDriver.cpp
@@ -1,12 +1,20 @@
 #include "DeviceDriver.h"
+#include <stdexcept>
+const int READ_REPETITION = 4;
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int value_init = (int) m_hardware->read(address);
+    for (int i = 0; i < READ_REPETITION; i++) {
+        int value_fresh = (int) m_hardware->read(address);
+        if (value_init != value_fresh)
+            throw std::exception("값이 달라여");
+    }
+
+    return value_init;
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test2/test.cpp
+++ Sample-Test2/test.cpp
@@ -1,9 +1,51 @@
 #pragma once
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../Project1/FlashMemoryDevice.h"
+#include "../Project1/DeviceDriver.cpp"
+#include <stdexcept>
 
 using namespace testing;
 
 TEST(TestCaseName, TestName) {
 	EXPECT_THAT(1, Eq(1));
+}
+
+class HwMock : public FlashMemoryDevice 
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+	
+};
+
+
+TEST(DeviceDriverTest, Read_0)
+{
+	HwMock flash_mock;
+	EXPECT_CALL(flash_mock, read(_)).Times(5);
+
+	DeviceDriver dd(&flash_mock);
+	long temp = dd.read(0x00);
+}
+
+TEST(DeviceDriverTest, Read_FAIL)
+{
+	HwMock flash_mock;
+	EXPECT_CALL(flash_mock, read(0x00))
+		.WillOnce(Return(2))
+		.WillRepeatedly(Return(3));
+
+	DeviceDriver dd(&flash_mock);
+	EXPECT_THROW(dd.read(0x00), std::exception);
+}
+
+TEST(DeviceDriverTest, Read_SUCCESS)
+{
+	HwMock flash_mock;
+	EXPECT_CALL(flash_mock, read(0x00))
+		.WillRepeatedly(Return(3));
+
+	DeviceDriver dd(&flash_mock);
+	EXPECT_THAT(dd.read(0x00), Eq(3));
 }
